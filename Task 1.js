afc2ec64304f8ad0f7eee23e1e864fcc0a1203ba
MaKankadze7 <kankadze.13@gmail.com>
2024-11-14 00:42:19 +0400

    Adding new file

---
+++ Task 1.js
@@ -0,0 +1,6 @@
+let number = prompt("Enter a number : ") ;
+number = parseFloat(number)
+
+if (number > 7) {
+    console.log("Hello") ;
+}
---
+++ Task 2.js
@@ -0,0 +1,6 @@
+let name = prompt("Enter your name:");
+if (name === John) {
+    console.log("Hello, John");
+} else {
+    console.log("There is no such name");
+}
